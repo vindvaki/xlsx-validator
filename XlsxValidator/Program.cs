87c9fbca9d3e5bc2833229be96b821c4d3aa1db7
Hörður Freyr Yngvason <hordur.yngvason@booking.com>
2017-03-28 21:28:58 +0200

    Accept multiple file names and return non-0 status on error

--- XlsxValidator/Program.cs
+++ XlsxValidator/Program.cs
@@ -10,37 +10,36 @@ namespace XlsxValidator
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+		public static int Main(string[] args)
 		{
+			var status = 0;
+
 			if (args.Count() == 0)
 			{
 				Console.WriteLine("No document given");
-				return;
+				return status;
 			}
-			var path = args[0];
-
-			using (SpreadsheetDocument doc = SpreadsheetDocument.Open(path, false))
+			foreach (var path in args)
 			{
-				var validator = new OpenXmlValidator();
-				var errors = validator.Validate(doc);
-				if (errors.Count() == 0)
+				using (SpreadsheetDocument doc = SpreadsheetDocument.Open(path, false))
 				{
-					Console.WriteLine("Document is valid");
-				}
-				else
-				{
-					Console.WriteLine("Document is not valid");
-				}
-				Console.WriteLine();
-				foreach (var error in errors)
-				{
-					Console.WriteLine("File: {0}", path);
-					Console.WriteLine("Error: {0}", error.Description);
-					Console.WriteLine("ContentType: {0}", error.Part.ContentType);
-					Console.WriteLine("XPath: {0}", error.Path.XPath);
-					Console.WriteLine();
+					var validator = new OpenXmlValidator();
+					var errors = validator.Validate(doc);
+					if (errors.Count() > 0)
+					{
+						status = 1;
+						foreach (var error in errors)
+						{
+							Console.WriteLine("File: {0}", path);
+							Console.WriteLine("Error: {0}", error.Description);
+							Console.WriteLine("ContentType: {0}", error.Part.ContentType);
+							Console.WriteLine("XPath: {0}", error.Path.XPath);
+							Console.WriteLine();
+						}
+					}
 				}
 			}
+			return status;
 		}
 	}
 }
